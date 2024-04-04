dc18dd0660634d7cc52acb641aacaaeecff88b2b
wongwh2002 <wenghongwong5686@gmail.com>
2024-04-04 22:26:34 +0800

    changed payload

--- payload.py
+++ payload.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
+OPEN_ANGLE = 35
+CLOSE_ANGLE = 110
 PAYLOAD_PIN = 5
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PAYLOAD_PIN, GPIO.OUT)
@@ -9,12 +11,12 @@ p = GPIO.PWM(PAYLOAD_PIN, 50)
 
 p.start(2.5)
 for i in range(5):
-    for j in range(90, 25, -2):
+    for j in range(CLOSE_ANGLE,OPEN_ANGLE, -3):
         change = j/10
         p.ChangeDutyCycle(change)
         time.sleep(0.1)
         print("open?")
-    for j in range(25, 90, 2):
+    for j in range(OPEN_ANGLE, CLOSE_ANGLE, 6):
         change = j/10
         p.ChangeDutyCycle(change)
         time.sleep(0.1)
