d21cd1861868ab9e95ee9ac6e15e6fbff5a5321c
namitdeb739 <namitdeb739@gmail.com>
2024-04-11 20:53:09 +0800

    Testing explore.py

--- lineFollower.py
+++ lineFollower.py
@@ -14,6 +14,7 @@ import time
 import subprocess
 import requests
 import json
+
 OPEN_ANGLE = 35
 CLOSE_ANGLE = 110
 PAYLOAD_PIN = 5
@@ -22,17 +23,18 @@ GPIO.setup(PAYLOAD_PIN, GPIO.OUT)
 GPIO.setwarnings(False)
 p = GPIO.PWM(PAYLOAD_PIN, 50)
 
+
 def payload():
     p.start(2.5)
     for i in range(1):
-        for j in range(CLOSE_ANGLE,OPEN_ANGLE, -3):
-            change = j/10
+        for j in range(CLOSE_ANGLE, OPEN_ANGLE, -3):
+            change = j / 10
             p.ChangeDutyCycle(change)
             time.sleep(0.1)
             print("open?")
         time.sleep(1)
         for j in range(OPEN_ANGLE, CLOSE_ANGLE, 6):
-            change = j/10
+            change = j / 10
             p.ChangeDutyCycle(change)
             time.sleep(0.1)
             print("close?")
@@ -41,28 +43,28 @@ def payload():
     p.stop()
     GPIO.cleanup()
 
+
 def door():
-    url = 'http://192.168.59.89/openDoor'
-    myobj = {
-            "action": "openDoor",
-            "parameters": {"robotId": "34"}
-            }
-    x = requests.post(url, json = myobj)
+    url = "http://192.168.59.89/openDoor"
+    myobj = {"action": "openDoor", "parameters": {"robotId": "34"}}
+    x = requests.post(url, json=myobj)
     x_dict = json.loads(x.text)
     status = x_dict["status"]
     door_num = x_dict["data"]["message"]
     return door_num
 
+
 LL_PIN = 19
 L_PIN = 6
 R_PIN = 13
 RR_PIN = 26
-rotatechange = -2.75/4 #max 2.8
-speedchange = 0.10 #max 0.22
+rotatechange = -2.75 / 4  # max 2.8
+speedchange = 0.10  # max 0.22
 stop_distance = 0.25
 firstCheck = True
 reverse = False
 # espTime = time.now()
+
 
 def GPIO_setup():
     GPIO.setwarnings(False)
@@ -72,13 +74,14 @@ def GPIO_setup():
     GPIO.setup(R_PIN, GPIO.IN)
     GPIO.setup(RR_PIN, GPIO.IN)
 
+
 class linerMover(Node):
     def __init__(self):
-        super().__init__('nav')
-        #publisher for moving TurtleBot
-        self.publisher_twist = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.publisher_bool = self.create_publisher(Bool, 'checkpoint', 10)
-        self.get_logger().info('Publisher for Twist')
+        super().__init__("nav")
+        # publisher for moving TurtleBot
+        self.publisher_twist = self.create_publisher(Twist, "cmd_vel", 10)
+        self.publisher_bool = self.create_publisher(Bool, "checkpoint", 10)
+        self.get_logger().info("Publisher for Twist")
         self.counter = 0
         self.x = 0.0
         self.z = 0.0
@@ -91,7 +94,7 @@ class linerMover(Node):
         self.publisher_twist.publish(twist)
 
     def turnRight(self):
-        self.get_logger().info('turnRight')
+        self.get_logger().info("turnRight")
         while GPIO.input(RR_PIN):
             self.x = -0.01
             self.z = -rotatechange
@@ -102,9 +105,9 @@ class linerMover(Node):
             self.publish()
 
     def turnLeft(self):
-        self.get_logger().info('turnLeftttttttt')
+        self.get_logger().info("turnLeftttttttt")
         self.z = rotatechange
-        self.x = speedchange/4
+        self.x = speedchange / 4
         while GPIO.input(LL_PIN):
             self.x = -0.01
             self.z = rotatechange
@@ -115,62 +118,62 @@ class linerMover(Node):
             self.publish()
 
     def moveStraight(self):
-        self.get_logger().info('stght')
+        self.get_logger().info("stght")
         self.x = speedchange
         self.z = 0.0
 
     def reverse(self):
         global innerSensor
         global outerSensor
-        self.get_logger().info('reverseeeeeee')
+        self.get_logger().info("reverseeeeeee")
         self.x = 0.01
         self.z = 0.0
         self.publish()
         nudge = 0
         first = True
-        while ([0, 0] != innerSensor):
+        while [0, 0] != innerSensor:
             innerSensor = [GPIO.input(L_PIN), GPIO.input(R_PIN)]
             outerSensor = [GPIO.input(LL_PIN), GPIO.input(RR_PIN)]
             print(f"innerrrr: {innerSensor}")
             print(f"outerrrr: {outerSensor}")
 
-            if (first and (innerSensor[0] or (outerSensor[0] and [0,0] == innerSensor))):
+            if first and (innerSensor[0] or (outerSensor[0] and [0, 0] == innerSensor)):
                 first = False
-                nudge = 0 #go left
-            if (first and (innerSensor[1] or (outerSensor[1] and [0,0] == innerSensor))):
+                nudge = 0  # go left
+            if first and (innerSensor[1] or (outerSensor[1] and [0, 0] == innerSensor)):
                 first = False
-                nudge = 1 #go right
-        self.get_logger().info('grhsbdruihgrdnughdr')
+                nudge = 1  # go right
+        self.get_logger().info("grhsbdruihgrdnughdr")
         self.x = 0.0
         print(f"nudge: {nudge}")
-        while (GPIO.input(LL_PIN) and GPIO.input(RR_PIN)):
-            if (nudge == 0):
-                self.z = rotatechange #nudgeleft
+        while GPIO.input(LL_PIN) and GPIO.input(RR_PIN):
+            if nudge == 0:
+                self.z = rotatechange  # nudgeleft
             else:
-                self.z =  -rotatechange #nudgeRight
+                self.z = -rotatechange  # nudgeRight
             self.get_logger().info("gebhgbresg")
             self.publish()
 
     def nudgeLeft(self):
-        self.get_logger().info('nudgeLeft')
-        self.x = speedchange/5
-        self.z = rotatechange/5
+        self.get_logger().info("nudgeLeft")
+        self.x = speedchange / 5
+        self.z = rotatechange / 5
 
     def nudgeRight(self):
-        self.get_logger().info('nudgeRight')
-        self.x = speedchange/5
-        self.z = -rotatechange/5
+        self.get_logger().info("nudgeRight")
+        self.x = speedchange / 5
+        self.z = -rotatechange / 5
 
     def checkPoint(self):
         global firstCheck, timed
-        self.get_logger().info('check')
+        self.get_logger().info("check")
         if firstCheck or (time.time() - timed > 2):
             timed = time.time()
             firstCheck = False
             self.counter += 1
-            self.get_logger().info('Checkpoint: ')
+            self.get_logger().info("Checkpoint: ")
             self.get_logger().info(str(self.counter))
-            if (self.counter == 1):
+            if self.counter == 1:
                 door_num = door()
                 if "1" in door_num:
                     turnLeft()
@@ -179,13 +182,11 @@ class linerMover(Node):
 
                 # espTime = time.now()
 
-            if (self.counter == 2):
+            if self.counter == 2:
                 payload()
 
-                
-
     def stopbot(self):
-        self.get_logger().info('In stopbot')
+        self.get_logger().info("In stopbot")
         # publish to cmd_vel to move TurtleBot
         self.x = 0.0
         self.z = 0.0
@@ -202,26 +203,26 @@ class linerMover(Node):
                 #     time.sleep(5)
                 #     self.publisher_bool.publish(True)
                 #     break
-                
+
                 innerSensor = [GPIO.input(L_PIN), GPIO.input(R_PIN)]
                 outerSensor = [GPIO.input(LL_PIN), GPIO.input(RR_PIN)]
                 print("----------------")
                 print(f"inner: {innerSensor}")
                 print(f"outer: {outerSensor}")
-                if ([1, 1] == innerSensor):
-                    if ([0, 0] == outerSensor):
+                if [1, 1] == innerSensor:
+                    if [0, 0] == outerSensor:
                         self.moveStraight()
-                    elif ([0, 1] == outerSensor):
+                    elif [0, 1] == outerSensor:
                         self.turnRight()
-                    elif ([1, 0] == outerSensor):
+                    elif [1, 0] == outerSensor:
                         self.turnLeft()
-                    elif ([1, 1] == outerSensor):
+                    elif [1, 1] == outerSensor:
                         self.checkPoint()
-                elif ([0, 1] == innerSensor):
+                elif [0, 1] == innerSensor:
                     self.nudgeRight()
-                elif ([1, 0] == innerSensor):
+                elif [1, 0] == innerSensor:
                     self.nudgeLeft()
-                elif ([0, 0] == innerSensor):
+                elif [0, 0] == innerSensor:
                     self.reverse()
                 self.publish()
                 print(f"self.x: {self.x}, self.z: {self.z}")
@@ -232,10 +233,11 @@ class linerMover(Node):
 
         # Ctrl-c detected
         finally:
-            #stop moving
+            # stop moving
             self.stopbot()
 
-def main(args = None):
+
+def main(args=None):
     rclpy.init(args=args)
     GPIO_setup()
     nav = linerMover()
@@ -245,6 +247,6 @@ def main(args = None):
     rclpy.shutdown()
     GPIO.cleanup()
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
